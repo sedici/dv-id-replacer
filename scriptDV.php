d8d3d9451cd91bc78dbe26debe94db13f682531c
Pablo Gabriel Terrone <pabloterrone05@gmail.com>
2023-11-23 11:54:38 -0300

    Agrego procesos y corrijo errores de sintaxis

--- scriptDV.php
+++ scriptDV.php
@@ -1,10 +1,33 @@
 <?php
-// Configuración de la base de datos
+// Variables de entorno 
 $host = getenv('DB_HOST');
 $usuario = getenv('DB_USER');
 $clave = getenv('DB_PASS');
 $base_de_datos = getenv('DB_NAME');
 echo $usuario;
+
+// Procesos auxiliares
+
+function listDataWithPerma($db_connection){
+
+}
+
+function listDatasetWithID($db_connection){
+    $query =   "SELECT  Id,dtype,authority,identifier,protocol,storageidentifier 
+                FROM public.dvobject
+                WHERE protocol is not null";
+    $result = pg_query($db_connection,$query);
+    if  (!$result) {
+        echo "query did not execute";
+    }
+     else{  
+        while ($row = pg_fetch_array($result)) {
+          var_dump($row);
+        }
+       }
+    return $result;
+}
+
 // Conexión a PostgreSQL
 $conexion_pgsql = pg_connect("host=localhost dbname=dataverse user=dataverse password=secret");
 
@@ -14,5 +37,13 @@ if (!$conexion_pgsql) {
 }
 else{
 	echo "Conexión establecida con la base de datos";
+    listDatasetWithID($conexion_pgsql);
 }
+
+$solrURL= "http://localhost:8983/solr/";
+
+
+// Cierro la conexión a la base de datos
+pg_close($conexion_pgsql);
+
 ?>
