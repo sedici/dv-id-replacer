5039ae01ca6b3010d3f0796d3699cc116f3f379a
Pablo Gabriel Terrone <pabloterrone05@gmail.com>
2023-12-04 11:56:29 -0300

    Func. para mover directorios + colores en output

--- scriptDV.php
+++ scriptDV.php
@@ -11,6 +11,11 @@ $idViejo = $argv[1];
 $prefijoNuevo = $argv[2];
 $sufijoNuevo = $argv[3];
 
+// Códigos de escape ANSI para colores
+$colorRojo = "\033[0;31m";
+$colorVerde = "\033[0;32m";
+$colorReset = "\033[0m";
+
 // Conexión a PostgreSQL
 $conexion_pgsql = pg_connect("host=localhost dbname=dataverse user=dataverse password=secret");
 
@@ -19,18 +24,18 @@ if (!$conexion_pgsql) {
     die("Error de conexión a PostgreSQL: " . pg_last_error());
 }
 else{
-	echo "Conexión establecida con la base de datos \n";
+	imprimirMensajeVerde("Conexión establecida con la base de datos");
     $datasetData = getDatasetID($conexion_pgsql,$idViejo);
     if(isset($datasetData['id'])){
         updateDataset($conexion_pgsql, $datasetData['id'], $prefijoNuevo, $sufijoNuevo);
         $prefijoViejo= $datasetData['authority'];
         $sufijoViejo= $datasetData['identifier'];
         refreshSolr($datasetData['id']);
-        moveFiles($prefijoViejo, $sufijoViejo, $prefijoNuevo, $sufijoNuevo);
+        changeLocation($prefijoViejo, $sufijoViejo, $prefijoNuevo, $sufijoNuevo);
 
     }
     else{
-        echo "No se encontro el registro indicado. \n";
+        imprimirMensajeRojo("No se encontro el registro indicado.");
     }
    
 }
@@ -48,7 +53,7 @@ function getDatasetID($db_connection, $temp_id){
     $result = pg_query($db_connection,$query);
 
     if  (!$result) {
-        echo "No se pudo ejecutar la consulta de la función 'getDatasetID' \n";
+        imprimirMensajeRojo("No se pudo ejecutar la consulta de la función 'getDatasetID'");
     }
      else{  
         $row = pg_fetch_array($result);
@@ -64,7 +69,7 @@ function listDatasetWithFake($db_connection){
                 WHERE protocol IN ('perma', 'fakedoi')";
     $result = pg_query($db_connection,$query);
     if  (!$result) {
-        echo "No se pudo ejecutar la consulta de la función 'listDatasetWithFake' \n";
+        imprimirMensajeRojo("No se pudo ejecutar la consulta de la función 'listDatasetWithFake'");
     }
      else{  
         while ($row = pg_fetch_array($result)) {
@@ -75,7 +80,6 @@ function listDatasetWithFake($db_connection){
 }
 
 function updateDataset($db_connection, $id, $prefijoNuevo, $sufijoNuevo){
-    var_dump($id);
     $query = "  UPDATE dvobject 
                 SET authority = '" . $prefijoNuevo . "', 
                 identifier = '" . $sufijoNuevo . "', 
@@ -85,10 +89,10 @@ function updateDataset($db_connection, $id, $prefijoNuevo, $sufijoNuevo){
     $result = pg_query($db_connection,$query);
     $state = pg_result_error($result);
     if  ($state) {
-        echo "No se pudo realizar la actualización: " . $state . "\n";
+        imprimirMensajeRojo("No se pudo realizar la actualización: '$state'");
     }
     else{  
-        echo "Se actualizo correctamente el registro con id ". $id . "\n";
+        imprimirMensajeVerde("Se actualizo correctamente el registro con id '$id'");
     }
 }
 
@@ -98,33 +102,72 @@ function refreshSolr($id){
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); // Devuelve el resultado como string 
     $result = curl_exec($curl);
     if (curl_errno($curl)) {
-        echo 'Error en la solicitud cURL para refrescar indice SOLR: ' . curl_error($curl) . "\n";
+        $msg = 'Error en la solicitud cURL para refrescar indice SOLR: ' . curl_error($curl);
+        imprimirMensajeRojo($msg );
     }
     else{
-        echo "Se refresco con éxito el indice del dataset "  . $id . "\n";
+        imprimirMensajeVerde("Se refresco con éxito el indice del dataset '$id'");
     }
     
     // Cerrar la sesión cURL
     curl_close($curl);
 }
 
-function moveFiles($oldPrefix, $oldSufix , $newPrefix, $newSufix){
-    $base_dir = 'files/';
-    $new_base_dir = $base_dir . $newPrefix;
-    // Creo el directorio base ( /files/authority )
+function changeLocation($oldPrefix, $oldSufix , $newPrefix, $newSufix){
+    $base_dir = '/home/pablo/Escritorio/dataverseDEV/docker-dev-volumes/app/data/store/';
+    $new_base_dir = $base_dir . $newPrefix . "/" . $newSufix;
+    // Creo el directorio base 
     if (!is_dir($new_base_dir)) {
         if (mkdir($new_base_dir, 0755, true)) {
-            echo 'La carpeta se ha creado exitosamente.';
+            imprimirMensajeVerde('La carpeta se ha creado exitosamente.');
         } else {
-            echo 'Hubo un error al intentar crear la carpeta.';
+            imprimirMensajeRojo('Hubo un error al intentar crear la carpeta.');
         }
     }
-    $new_full_dir = $new_base_dir . "/" . $newSufix;
     $old_full_dir = $base_dir . $oldPrefix . "/" . $oldSufix;
-    if (rename($old_full_dir, $new_full_dir)) {
-        echo 'La carpeta se ha movido exitosamente.';
-    } else {
-        echo 'Hubo un error al intentar mover la carpeta.';
-    }
+    moveFiles($old_full_dir, $new_base_dir);
+
 }
+
+function moveFiles($originFolder, $destinationFolder){
+    $archivos = scandir($originFolder);
+    foreach ($archivos as $archivo) {
+        // Excluyo los directorios "." y ".."
+        if ($archivo != "." && $archivo != "..") {
+            $rutaArchivoOrigen = $originFolder . '/' . $archivo;
+            $rutaArchivoDestino = $destinationFolder . '/' . $archivo;
+
+            // Verifico si es un archivo o una carpeta
+            if (is_file($rutaArchivoOrigen)) {
+                // Muevo el archivo
+                if (rename($rutaArchivoOrigen, $rutaArchivoDestino)) {
+                    imprimirMensajeVerde("El archivo '$archivo' se ha movido exitosamente");
+                } else {
+                    imprimirMensajeRojo("Hubo un error al mover el archivo '$archivo'");
+                }
+            } elseif (is_dir($rutaArchivoOrigen)) {
+                // Muevo la carpeta
+                if (rename($rutaArchivoOrigen, $rutaArchivoDestino)) {
+                    imprimirMensajeVerde("La carpeta '$archivo' se ha movido exitosamente");
+                } else {
+                    imprimirMensajeRojo("Hubo un error al mover la carpeta '$archivo'");
+                }
+            }
+        }
+    }
+
+    imprimirMensajeVerde("Todos los archivos se movieron.");
+}
+
+function imprimirMensajeRojo($mensaje) {
+    global $colorRojo, $colorReset;
+    echo $colorRojo . $mensaje . $colorReset . "\n";
+}
+
+function imprimirMensajeVerde($mensaje) {
+    global $colorVerde, $colorReset;
+    echo $colorVerde . $mensaje . $colorReset . "\n";
+}
+
+
 ?>
